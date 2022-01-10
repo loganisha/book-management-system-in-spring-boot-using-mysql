b72c6666d5894928bd00080d920be2e6e70cde6f
loganisha <69141688+loganisha@users.noreply.github.com>
2022-01-10 16:06:37 +0530

    Add files via upload

---
+++ src/test/java/a/BookMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
